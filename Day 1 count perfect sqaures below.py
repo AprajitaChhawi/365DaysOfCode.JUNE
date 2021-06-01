cb64b83430d7a979537f601de38cbafe5ee3bc90
AprajitaChhawi <achhawip@gmail.com>
2021-06-01 23:22:08 +0530

    Create Day 1 count perfect sqaures below.py

---
+++ Day 1 count perfect sqaures below.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+from math import sqrt
+class Solution:
+    def countSquares(self, N):
+        return int(sqrt(N-1))
+        
+        
+        # code here 
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import math
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.countSquares(N))
+# } Driver Code Ends
