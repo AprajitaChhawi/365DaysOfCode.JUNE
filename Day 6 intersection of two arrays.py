38bf3277106959c029dec3fcf89a54ea4a1d7853
AprajitaChhawi <achhawip@gmail.com>
2021-06-24 22:46:12 +0530

    Create Day 6 intersection of two arrays.py

---
+++ Day 6 intersection of two arrays.py
@@ -0,0 +1,58 @@
+#User function Template for python3
+
+#Function to return the count of the number of elements in
+#the intersection of two arrays.
+class Solution:
+    def NumberofElementsInIntersection(self,a, b, n, m):
+        count=0
+        d={}
+        for i in a:
+            if i not in d:
+                d[i]=1
+        for i in set(b):
+            if i in d:
+                count=count+1
+        return count
+        #return: expected length of the intersection array.
+        
+        #code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        ob = Solution()
+        
+        print(ob.NumberofElementsInIntersection(a,b,n,m))
+        
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+                
+# } Driver Code Ends
