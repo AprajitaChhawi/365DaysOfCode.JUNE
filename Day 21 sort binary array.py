f36cc9977014226641e01f7b358e32581003265d
AprajitaChhawi <achhawip@gmail.com>
2021-07-12 23:50:55 +0530

    Create Day 21 sort binary array.py

---
+++ Day 21 sort binary array.py
@@ -0,0 +1,14 @@
+def segregateeven(l,n):
+    i=-1
+    for j in range(0,n):
+        if l[j]==0:
+            i=i+1
+            l[i],l[j]=l[j],l[i]
+    return l
+t=int(input())
+while t:
+    t=t-1
+    n=int(input())
+    l=list(map(int,input().split()))
+    l=segregateeven(l,n)
+    print(l)
